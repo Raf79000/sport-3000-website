771eb3cc2fe9328f5e0a6516fd16c3bd9749c28d
Raf79000 <daigre.raphael@gmail.com>
2025-03-25 11:34:23 +0100

    refactor: routes to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './pages/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./pages/App";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Checkout from "./pages/Checkout";
+import { CartProvider } from "./contexts/CartContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </CartProvider>
+    </Router>
   </React.StrictMode>
 );
 
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,32 +1,16 @@
-import '../styles/App.css';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Products from './Products';
-import About from './About';
-import Contact from './Contact';
-import Checkout from './Checkout';
-import { CartProvider } from '../contexts/CartContext';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import "../styles/App.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import ShoppingList from "../components/ShoppingList";
+import Cart from "../components/Cart";
 
 function App() {
   return (
     <div className="App">
-      <CartProvider>
-        <Router>
-          <Header />
-          <Routes>
-            <Route path='/' element={<Products />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/checkout' element={<Checkout />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </CartProvider>
+      <Header />
+      <ShoppingList />
+      <Cart />
+      <Footer />
     </div>
   );
 }
