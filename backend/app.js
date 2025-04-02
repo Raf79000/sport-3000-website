509e052698bda6236825acd79a13ae6cfdac96ed
Raf79000 <daigre.raphael@gmail.com>
2025-04-02 14:51:31 +0200

    feat: mysql db init

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
 const app = express();
+
+const db_connexion = mysql.createConnection({
+  host: "mariadb",
+  user: "user",
+  password: "eseo",
+  database: "sport_3000",
+});
+
+db_connexion.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à MySQL :", err);
+  } else {
+    console.log("Connexion à MySQL réussie !");
+  }
+});
 
 app.use(cors());
 
-app.use((req, res) =>{
+app.use((req, res) => {
   res.json({
-    message: 'Hello World!'
-  })
+    message: "Hello World!",
+  });
 });
 
 module.exports = app;
